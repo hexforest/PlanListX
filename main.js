ff3f705aaf2c4a0732e15f489e560eed4202dbdf
hexforest <hexforest.com@gmail.com>
2025-07-02 22:45:57 +0100

    update in local env  and working build

--- main.js
+++ main.js
@@ -1,26 +1,23 @@
 const { app, BrowserWindow } = require('electron');
-const path = require('path');
 
-function createWindow () {
+function createWindow() {
   const win = new BrowserWindow({
     width: 1000,
     height: 700,
     webPreferences: {
-      preload: path.join(__dirname, 'preload.js'),
+      nodeIntegration: true,
+      contextIsolation: false,
     },
   });
 
-  // Load the React bundle via file:// URL
-  win.loadFile('dist/index.html');
+  if (process.env.NODE_ENV === 'development') {
+    win.loadURL('http://localhost:3000');
+  } else {
+    win.loadFile('dist/index.html');
+  }
 }
 
-app.whenReady().then(() => {
-  createWindow();
-
-  app.on('activate', () => {
-    if (BrowserWindow.getAllWindows().length === 0) createWindow();
-  });
-});
+app.whenReady().then(createWindow);
 
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') app.quit();
